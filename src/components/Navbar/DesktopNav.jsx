3103a9422830d9d32ef4287daf5ca23f22ed2f97
Tobi <tobiloba.oyedele05@gmail.com>
2025-11-12 11:26:15 +0100

    Navbar done

--- src/components/Navbar/DesktopNav.jsx
+++ src/components/Navbar/DesktopNav.jsx
@@ -1,5 +1,28 @@
+// eslint-disable-next-line no-unused-vars
+import { motion } from "motion/react";
+
 const DesktopNav = () => {
-  return <nav>DesktopNav</nav>;
+  return (
+    <nav className="fixed w-full bg-gray-900/95">
+      <div className="flex items-center justify-between px-4 h-16">
+        <div className="text-2xl font-bold bg-linear-to-r from-cyan-400 to-purple-500 bg-clip-text text-transparent">
+          {"<Dev/>"}
+        </div>
+        <div className="space-x-8 text-white">
+          {["Home", "About", "Projects", "Skills", "Contact"].map((item) => (
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              key={item}
+              className="cursor-pointer font-semibold hover:text-cyan-400"
+            >
+              {item}
+            </motion.button>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default DesktopNav;
--- src/components/Navbar/MobileNav.jsx
+++ src/components/Navbar/MobileNav.jsx
@@ -1,11 +1,13 @@
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
+// eslint-disable-next-line no-unused-vars
+import { motion, AnimatePresence } from "motion/react";
 
 const MobileNav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="fixed w-full z-50">
+    <nav className="fixed w-full z-50 bg-gray-900/95">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="text-2xl font-bold bg-linear-to-r from-cyan-400 to-purple-500 bg-clip-text text-transparent">
@@ -13,7 +15,7 @@ const MobileNav = () => {
           </div>
 
           <button
-            className="md:hidden"
+            className="text-white"
             onClick={() => setIsMenuOpen(!isMenuOpen)}
           >
             {isMenuOpen ? <X /> : <Menu />}
@@ -21,18 +23,26 @@ const MobileNav = () => {
         </div>
       </div>
 
-      {isMenuOpen && (
-        <div className="md:hidden bg-gray-800 border-t border-gray-700">
-          {["Home", "About", "Projects", "Skills", "Contact"].map((item) => (
-            <button
-              key={item}
-              className="block w-full text-left px-4 py-3 hover:bg-gray-700 transition-colors"
-            >
-              {item}
-            </button>
-          ))}
-        </div>
-      )}
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+            className="bg-gray-800 border-t border-gray-700"
+          >
+            {["Home", "About", "Projects", "Skills", "Contact"].map((item) => (
+              <motion.button
+                key={item}
+                className="block w-full text-left text-white px-6 py-3"
+              >
+                {item}
+              </motion.button>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
