35c2d128cbcbeca70886a6fce19b775739cfe78f
Tobi <tobiloba.oyedele05@gmail.com>
2025-12-16 11:30:41 +0100

    Changed Oris Crown Link

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     id: 1,
     title: "Oris Crown Logistics | Global Freight Forwarding & Cargo Solutions",
     image: "/images/Oris_Crown_Logistics.png",
-    liveUrl: "https://oris-crown-logistics-nine.vercel.app/",
+    liveUrl: "https://www.orislogistics.com/",
     tech: ["Next.js", "TypeScript", "Framer Motion", "Tailwind CSS", "Vercel"],
   },
   {
