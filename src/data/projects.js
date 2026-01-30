50faa894bc55676e9b9f2db8d3335645e93bbb73
Tobi-Oyedele <tobiloba.oyedele05@gmail.com>
2026-01-30 11:07:10 +0100

    removed dictionary project

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,16 +8,9 @@ export const projects = [
   },
   {
     id: 2,
-    title: "Dictionary Web App",
-    image: "/images/Dictionary_Web_App.png",
-    liveUrl: "https://dictionary-web-app-pearl.vercel.app/",
-    tech: ["React", "TypeScript", "Tailwind CSS", "Vercel"],
-  },
-  {
-    id: 3,
     title: "AMSAfrica",
     image: "/images/Amsa.png",
     liveUrl: "https://www.amsafrica.org/",
-    tech: ["React", "TypeScript", "Tailwind CSS", "Vercel"],
+    tech: ["Next.js", "React", "TypeScript", "Tailwind CSS", "Vercel"],
   },
 ];
